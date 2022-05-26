421702b8d17f0820726c1a5d4528f027cf41861d
jarednjk <jarednjk@gmail.com>
2022-05-26 08:01:17 +0000

    improve q4 ans

--- hands-on/q4-simple-json-example/script.js
+++ hands-on/q4-simple-json-example/script.js
@@ -1,9 +1,23 @@
-async function run() {
-    let response = await axios.get('artwork.json');
-    document.querySelector('#load-image-btn').addEventListener('click', function(){
-        document.querySelector('#artwork').innerHTML = `<h1>${response.data.title}</h1>`;
-        document.querySelector('#artwork').innerHTML += `<img src='${response.data.image_url}'>`;
-    })
-}
+// async function run() {
+//     let response = await axios.get('artwork.json');
+//     document.querySelector('#load-image-btn').addEventListener('click', function(){
+//         document.querySelector('#artwork').innerHTML = `<h1>${response.data.title}</h1>`;
+//         document.querySelector('#artwork').innerHTML += `<img src='${response.data.image_url}'>`;
+//     })
+// }
 
-run();
+// run();
+
+document.querySelector('#load-image-btn').addEventListener('click', async()=>{
+    let response = await axios.get('artwork.json');
+
+    let h2Element = document.createElement('h2');
+    h2Element.innerHTML = response.data.title;
+
+    let imageElement = document.createElement('img');
+    imageElement.src = response.data.image_url;
+
+    let artworkDiv = document.querySelector('#artwork');
+    artworkDiv.appendChild(h2Element);
+    artworkDiv.appendChild(imageElement);
+})
