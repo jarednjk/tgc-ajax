618d30c59986644bde37e36fde418c185523ca32
jarednjk <jarednjk@gmail.com>
2022-05-28 06:09:28 +0000

    fixed q5 ans

--- hands-on/q5-array-of-json-objects/script.js
+++ hands-on/q5-array-of-json-objects/script.js
@@ -1,46 +1,19 @@
-// async function run() {
-//     let response = await axios.get('users.json');
-//     document.querySelector('#load-users-btn').addEventListener('click', function(){
-//         console.log(response.data)
-//         for (let i =0; i < users.length; i++)
-//         document.querySelector('#all-users').innerHTML += `<h1>${response.data.users[i].firstName}</h1>`;
-//     })
-// }
-
-// run();
-
 document.querySelector('#load-users-btn').addEventListener('click', async ()=>{
 
     let response = await axios.get('users.json');
+    let allUsersDiv = document.querySelector('#all-users');
 
-    let firstNameTitle = document.createElement('h2')
-    firstNameTitle.innerHTML = "First Name";
-    let lastNameTitle = document.createElement('h2')
-    lastNameTitle.innerHTML = "Last Name";
-    let emailTitle = document.createElement('h2')
-    emailTitle.innerHTML = "Email";
-
-    let firstNameUL = document.createElement('ul');
-    let lastNameUL = document.createElement('ul');
-    let emailUL = document.createElement('ul');
-    for (let i=0; i <response.data.users.length; i++) {
-        firstNameUL.innerHTML += `<li>${response.data.users[i].firstName}</li>`;
-        lastNameUL.innerHTML += `<li>${response.data.users[i].lastName}</li>`;
-        emailUL.innerHTML += `<li>${response.data.users[i].emailAddress}</li>`;
+    for (let u of response.data.users) {
+        let ulElement = document.createElement('ul');
+        let firstNameElement = document.createElement('li');
+        firstNameElement.innerHTML = u.firstName;
+        ulElement.appendChild(firstNameElement);
+        let lastNameElement = document.createElement('li');
+        lastNameElement.innerHTML = u.lastName;
+        ulElement.appendChild(lastNameElement);
+        let emailElement = document.createElement('li');
+        emailElement.innerHTML = u.emailAddress;
+        ulElement.appendChild(emailElement);
+        allUsersDiv.appendChild(ulElement);
     }
-    // let allUsersDiv = document.querySelector('#all-users');
-    let firstNameDiv = document.querySelector('#first-name');
-    firstNameDiv.appendChild(firstNameTitle);
-    firstNameDiv.appendChild(firstNameUL);
-
-    let lastNameDiv = document.querySelector('#last-name');
-    lastNameDiv.appendChild(lastNameTitle);
-    lastNameDiv.appendChild(lastNameUL);
-
-    let emailDiv = document.querySelector('#email');
-    emailDiv.appendChild(emailTitle)
-    emailDiv.appendChild(emailUL);
-    
-
-    console.log(response.data);
 })
