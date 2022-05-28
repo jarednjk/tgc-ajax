d19db6bab9a4ef9a477701820c11851938bc98aa
jarednjk <jarednjk@gmail.com>
2022-05-28 06:09:20 +0000

    edit q3 ans

--- hands-on/q3-combine-multiple-files/script.js
+++ hands-on/q3-combine-multiple-files/script.js
@@ -1,9 +1,15 @@
 document.querySelector('#load-btn').addEventListener('click', async()=>{
-    let response1 = await axios.get('file1.txt');
-    let response2 = await axios.get('file2.txt');
+    let request1 = axios.get('file1.txt');
+    let request2 = axios.get('file2.txt');
+
+    let response1 = await request1;
+    let response2 = await request2;
     console.log(response1.data);
     console.log(response2.data);
 
-    document.querySelector('#content').innerHTML += response1.data;
-    document.querySelector('#content').innerHTML += `\n\n${response2.data}`;
+    document.querySelector('#content').innerHTML = response1.data + response2.data;
+
+    // Shortcut
+    // let responses = await axios.all([axios.get('file1.txt'), axios.get('file2.txt')])
 })
+
